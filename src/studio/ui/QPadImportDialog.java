6cbd24c0c43f001a2e0ed49e0fb072d80440e38f
dzmipt <dz.mipt@gmail.com>
2021-03-05 18:57:14 +0300

    QPadImportDialog initial version

---
+++ src/studio/ui/QPadImportDialog.java
@@ -0,0 +1,163 @@
+package studio.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class QPadImportDialog extends EscapeDialog {
+
+    private JTextField txtServers;
+    private JRadioButton btnOverwrite, btnPrepend, btnAppend;
+    private JTextField txtRootName;
+
+    private JFileChooser chooser;
+
+    public enum Location {Overwrite, Prepend, Append}
+
+    public QPadImportDialog(JFrame parent) {
+        super(parent, "QPad Server List Import");
+        initComponents();
+    }
+
+    public String getServersCfgLocation() {
+        return txtServers.getText();
+    }
+
+    public Location getImportTo() {
+        if (btnOverwrite.isSelected()) return Location.Overwrite;
+        if (btnPrepend.isSelected()) return Location.Prepend;
+        if (btnAppend.isSelected()) return Location.Append;
+
+        return null;
+    }
+
+    public String getRootName() {
+        return txtRootName.getText();
+    }
+
+    private void initComponents() {
+        JLabel lblServers = new JLabel("Select location of Servers.cfg:     ");
+        txtServers = new JTextField();
+        JButton btnServers = new JButton("...");
+
+        JLabel lblLocation = new JLabel("Location where servers will be imported in the current Server Tree:     ");
+        btnOverwrite = new JRadioButton("Overwrite");
+        btnPrepend = new JRadioButton("Prepend");
+        btnAppend = new JRadioButton("Append");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(btnOverwrite);
+        buttonGroup.add(btnAppend);
+        buttonGroup.add(btnPrepend);
+
+        JLabel lblRootName = new JLabel("Root name of importer server or leave it empty:     ");
+        txtRootName = new JTextField();
+
+        JPanel main = new JPanel();
+        GroupLayout layout = new GroupLayout(main);
+        main.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        layout.setHorizontalGroup(
+                layout.createParallelGroup()
+                    .addGroup(
+                            layout.createSequentialGroup()
+                                .addComponent(lblServers)
+                                .addComponent(txtServers)
+                                .addComponent(btnServers)
+                    ).addGroup(
+                            layout.createSequentialGroup()
+                                .addComponent(lblLocation)
+                                .addComponent(btnOverwrite)
+                                .addComponent(btnPrepend)
+                                .addComponent(btnAppend)
+                    ).addGroup(
+                            layout.createSequentialGroup()
+                                .addComponent(lblRootName)
+                                .addComponent(txtRootName)
+                    )
+        );
+
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                    .addGroup(
+                        layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblServers)
+                                .addComponent(txtServers)
+                                .addComponent(btnServers)
+                    ).addGroup(
+                        layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblLocation)
+                                .addComponent(btnOverwrite)
+                                .addComponent(btnPrepend)
+                                .addComponent(btnAppend)
+                    ).addGroup(
+                        layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblRootName)
+                                .addComponent(txtRootName)
+                    )
+        );
+
+        JButton btnImport = new JButton("Import");
+        JButton btnCancel = new JButton("Cancel");
+        JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        bottom.add(btnImport);
+        bottom.add(btnCancel);
+
+        JPanel root = new JPanel(new BorderLayout());
+        root.add(main, BorderLayout.CENTER);
+        root.add(bottom, BorderLayout.SOUTH);
+        setContentPane(root);
+
+        chooser = new JFileChooser();
+        btnImport.addActionListener(e->checkAndAccept());
+        btnCancel.addActionListener(e->cancel());
+        btnServers.addActionListener(e->selectServersCfg());
+    }
+
+    private void selectServersCfg() {
+        int result = chooser.showOpenDialog(this);
+        if (result == JFileChooser.ERROR_OPTION) {
+            JOptionPane.showMessageDialog(this, "Error while selecting Servers.cfg",
+                                        "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if (result == JFileChooser.CANCEL_OPTION) {
+            return;
+        }
+
+        String location = chooser.getSelectedFile().getAbsoluteFile().toString();
+        txtServers.setText(location);
+    }
+
+    private void checkAndAccept() {
+        String location = getServersCfgLocation();
+        if (location.trim().length() == 0) {
+            JOptionPane.showMessageDialog(this, "File to import is not selected",
+                    "Not Found Servers.cfg", JOptionPane.ERROR_MESSAGE);
+            return;
+
+        }
+
+        if (! new File(location).exists()) {
+            JOptionPane.showMessageDialog(this, "File to import is not exist (" + location + ")",
+                                "Not Found Servers.cfg", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (getImportTo() == null) {
+            JOptionPane.showMessageDialog(this, "Select how current Server Tree should be modified: Overwrite/Prepend/Append",
+                    "Where to Import", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        accept();
+    }
+
+    public static void main(String[] args) {
+        QPadImportDialog d = new QPadImportDialog(null);
+
+        d.pack();
+        d.setVisible(true);
+    }
+}
