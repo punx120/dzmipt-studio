8ea7cc74c8f920ae52f2db79a5ef8c7d05c4aa7d
dzmipt <dz.mipt@gmail.com>
2021-04-14 20:09:01 +0300

    update test data

--- test/studio/utils/QPadConverterTest.java
+++ test/studio/utils/QPadConverterTest.java
@@ -13,54 +13,54 @@ public class QPadConverterTest {
     public void testConvert() {
         Credentials cred = new Credentials("aUser", "aPassword");
 
-        Server server = QPadConverter.convert("`:server.uk.db.com:11223`name", "authMethod", cred);
-        assertEquals("server.uk.db.com", server.getHost());
+        Server server = QPadConverter.convert("`:server.com:11223`name", "authMethod", cred);
+        assertEquals("server.com", server.getHost());
         assertEquals(11223, server.getPort());
         assertEquals("name", server.getName());
         assertEquals("authMethod", server.getAuthenticationMechanism());
         assertEquals(cred.getUsername(), server.getUsername());
         assertEquals(cred.getPassword(), server.getPassword());
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223`Parent`folder`name", "auth", cred);
+        server = QPadConverter.convert("`:server.com:11223`Parent`folder`name", "auth", cred);
         assertEquals("Parent/folder/name", server.getFullName());
         assertEquals("name", server.getName());
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223:user:password`Parent`folder`name", "auth", cred);
+        server = QPadConverter.convert("`:server.com:11223:user:password`Parent`folder`name", "auth", cred);
         assertEquals("Parent/folder/name", server.getFullName());
         assertEquals("user", server.getUsername());
         assertEquals("password", server.getPassword());
         assertEquals(DefaultAuthenticationMechanism.NAME, server.getAuthenticationMechanism());
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223:user:testAuth?password`Parent`folder`name", "auth", cred);
+        server = QPadConverter.convert("`:server.com:11223:user:testAuth?password`Parent`folder`name", "auth", cred);
         assertEquals("Parent/folder/name", server.getFullName());
         assertEquals("user", server.getUsername());
         assertEquals("testAuth?password", server.getPassword());
         assertEquals("testAuth", server.getAuthenticationMechanism());
 
 
-        server = QPadConverter.convert("#`:server.uk.db.com:11223:user:auth?password`Parent`folder`name", "auth", cred);
+        server = QPadConverter.convert("#`:server.com:11223:user:auth?password`Parent`folder`name", "auth", cred);
         assertEquals(null, server);
 
         server = QPadConverter.convert("","auth", cred);
         assertEquals(null, server);
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223","auth", cred);
+        server = QPadConverter.convert("`:server.com:11223","auth", cred);
         assertEquals(null, server);
 
-        server = QPadConverter.convert("server.uk.db.com:11223`root`folder`somename","auth", cred);
+        server = QPadConverter.convert("server.com:11223`root`folder`somename","auth", cred);
         assertEquals(null, server);
 
-        server = QPadConverter.convert("`:server.uk.db.com:port`name","auth", cred);
+        server = QPadConverter.convert("`:server.com:port`name","auth", cred);
         assertEquals(null, server);
 
-        server = QPadConverter.convert("`:server.uk.db.com`name","auth", cred);
+        server = QPadConverter.convert("`:server.com`name","auth", cred);
         assertEquals(null, server);
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223:user`name","auth", cred);
+        server = QPadConverter.convert("`:server.com:11223:user`name","auth", cred);
         assertEquals("", server.getPassword());
         assertEquals("user", server.getUsername());
 
-        server = QPadConverter.convert("`:server.uk.db.com:11223`folder``server","auth", cred);
+        server = QPadConverter.convert("`:server.com:11223`folder``server","auth", cred);
         assertEquals("[empty]", server.getFolder().getFolder());
 
     }
